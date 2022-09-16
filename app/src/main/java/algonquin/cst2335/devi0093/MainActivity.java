ddfaf09d15957d3de8487e9057feb75751c79881
Alka Devi <alkadevi012@mail.com>
2022-09-16 10:37:33 -0400

    Initial commit

---
+++ app/src/main/java/algonquin/cst2335/devi0093/MainActivity.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.devi0093;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //loads the XML file on the screen:
+        setContentView(R.layout.activity_main);
+    }
+}
