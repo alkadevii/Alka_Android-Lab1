fbd5ddb3c50c63494f8fbcfc2a308700bbc1af89
Alka Devi <alkadevi012@mail.com>
2022-10-06 18:08:07 -0400

    initial

--- app/src/main/java/algonquin/cst2335/devi0093/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/devi0093/MainActivity.java
@@ -2,60 +2,34 @@ package algonquin.cst2335.devi0093;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import algonquin.cst2335.devi0093.databinding.ActivityMainBinding;
-import algonquin.cst2335.devi0093.R;
-import algonquin.cst2335.devi0093.databinding.ActivityMainBinding;
-import data.MainActivityViewModel;
-import data.MainActivityViewModel;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imgView;
-    Switch sw;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        imgView = findViewById(R.id.flagView);
-        sw = findViewById(R.id.Switch1);
-
-        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
-
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
-
-        });
+    protected void onStart() {
+        super.onStart();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
     }
 
-//
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( "MainActivity", "In onDestroy() - Destroying widgets" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( "MainActivity", "In onPause() - Pausing Widgets" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( "MainActivity", "In onResume() - Resume Widgets" );
+    }
+    //
 }
