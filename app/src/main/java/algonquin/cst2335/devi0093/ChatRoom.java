e5e559facd9811e5a6085bdbe2e4910512b6b80b
Alka Devi <alkadevi012@mail.com>
2022-11-02 11:00:00 -0400

    first commit

---
+++ app/src/main/java/algonquin/cst2335/devi0093/ChatRoom.java
@@ -0,0 +1,48 @@
+package algonquin.cst2335.devi0093;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import algonquin.cst2335.devi0093.databinding.ActivityChatRoomBinding;
+
+public class ChatRoom extends AppCompatActivity {
+    ActivityChatRoomBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityChatRoomBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.recycleView.setAdapter(new RecyclerView.Adapter<MyRowHolder>() {
+            @NonNull
+            @Override
+            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        });
+
+
+        }
+    class MyRowHolder extends RecyclerView.ViewHolder {
+        public MyRowHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
+
+}
